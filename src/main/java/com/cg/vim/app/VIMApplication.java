1066fe6f03b063fe4e371118ab2df94b447fcf83
aditya-badireddy <aditya.v.badireddy@gmail.com>
2017-12-10 16:00:10 +0530

    - supports the deployment as war file on tomcat

--- src/main/java/com/cg/vim/app/VIMApplication.java
+++ src/main/java/com/cg/vim/app/VIMApplication.java
@@ -2,13 +2,20 @@ package com.cg.vim.app;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.Import;
 
 import com.cg.vim.configuration.JpaConfiguration;
 
 @Import(JpaConfiguration.class)
 @SpringBootApplication(scanBasePackages={"com.cg.vim"})
-public class VIMApplication {
+public class VIMApplication extends SpringBootServletInitializer {
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(VIMApplication.class);
+    }
 
 	public static void main(String[] args) {
 		SpringApplication.run(VIMApplication.class, args);
